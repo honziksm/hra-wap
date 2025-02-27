093dfce2d3ca96adeb2f816b4ef5919ff096c56d
honzik <simekjan1190@gmail.com>
2025-02-27 08:27:55 +0100

    init

---
+++ res/js/main.js
@@ -0,0 +1,52 @@
+class Player {
+
+    constructor(x, y, w, h, c){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+        this.v = 4;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.c;
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+    }
+
+    update(keys) {
+        if (keys["w"]) this.y -= this.v;
+        if (keys["s"]) this.y += this.v;
+        if (keys["a"]) this.x -= this.v;
+        if (keys["d"]) this.x += this.v;
+    }
+}
+
+const myPlayer = new Player(10, 10, 50, 50, "green");
+
+const gameCanvas = document.getElementById("gameCanvas");
+const ctx = gameCanvas.getContext("2d");
+
+const resizeCanvas = () => {
+    gameCanvas.width = window.innerWidth;
+    gameCanvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+let keys = {};
+
+window.addEventListener("keydown", (e) => keys[e.key] = true);
+window.addEventListener("keyup", (e) => keys[e.key] = false);
+
+const gameLoop = () => {
+    ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height,)
+
+      myPlayer.update(keys);
+
+    myPlayer.draw(ctx);
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
