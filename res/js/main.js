fd22d9e9f00769abf3dd8080c2bd4324a07e4172
honzik <simekjan1190@gmail.com>
2025-03-06 09:24:34 +0100

    zmena

--- res/js/main.js
+++ res/js/main.js
@@ -7,6 +7,8 @@ class Player {
         this.h = h;
         this.c = c;
         this.v = 4;
+        this.bV = 4;
+
     }
 
     draw(ctx) {
@@ -15,10 +17,46 @@ class Player {
     }
 
     update(keys) {
-        if (keys["w"]) this.y -= this.v;
-        if (keys["s"]) this.y += this.v;
-        if (keys["a"]) this.x -= this.v;
-        if (keys["d"]) this.x += this.v;
+        if (keys["w"]) {
+            if (this.y - this.v > 0) {
+                this.y -= this.v; 
+            }
+           
+        }
+        if (keys["s"]) {
+            if  (this.y + this.h + this.v < gameCanvas.height) {
+               this.y += this.v;  
+            }
+           
+        }
+        if (keys["a"]) {
+            if (this.x - this.v > 0) {
+                 this.x -= this.v;
+            }
+           
+        }
+        if (keys["d"]) {
+            if (this.x + this.w + this.v < gameCanvas.width) {
+                this.x += this.v;
+            }        
+        }
+        if (keys[" "]){
+            this.v = 10;
+        } else {
+        this.v = this.bV
+    }
+        if (keys["e"]) {
+            if(this.w < 200 || this.h < 200){
+                this.w++;
+                this.h++  
+             }
+          }
+        if (keys["r"]) {
+            if(this.w > 30 || this.h > 30){
+               this.w--;
+               this.h--  
+            }
+         }
     }
 }
 
@@ -44,9 +82,27 @@ const gameLoop = () => {
     ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height,)
 
       myPlayer.update(keys);
-
+    checkCollision(myPlayer, testObject);
     myPlayer.draw(ctx);
+    ctx.fillStyle = testObject.c;
+    ctx.fillRect(testObject.x, testObject.y, testObject.w, testObject.h);
     requestAnimationFrame(gameLoop);
-}
+};
 
 requestAnimationFrame(gameLoop);
+
+let testObject = {
+    x: 100,
+    y: 200,
+    w: 100,
+    h: 100,
+    c: "black"
+}
+const checkCollision = (object1, object2) => {
+    if (object1.x + object1.w > object2.x && object1.x < object2.x + object2.w && object1.y + object1.h > object2.y && object1.y < object2.y + object2.h){
+          
+        console.log("nastala kolize");
+        object1.c = "red";
+    }
+
+}
